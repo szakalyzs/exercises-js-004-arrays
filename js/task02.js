051079189a7b0d741397b11cc303d97e338222a8
unknown <szakalyzs@gmail.com>
2020-11-24 20:08:00 +0100

    Two in three is done.

---
+++ js/task02.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const mixArr = [45, 'hello', true, 122, 0, 'A', '5', 7.83, false];
+const primitive = 0;
+
+const resultObj = (paramArr, paramValue) => {
+    return {
+        exsist: paramArr.some(item => item === paramValue),
+        index: paramArr.findIndex(item => item === paramValue),
+        allElementIsANumber: paramArr.every(item => typeof (item) === "number"),
+        someElementIsANumber: paramArr.some(item => typeof (item) === "number")
+    }
+};
+
+console.log(resultObj(mixArr, primitive));
